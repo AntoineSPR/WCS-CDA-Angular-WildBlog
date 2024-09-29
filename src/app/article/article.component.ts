9088a3a095a3c07adb185106b0ffbeb900b95350
AntoineSPR <antoine.simper@gmail.com>
2024-09-29 18:50:34 +0200

    Add dynamic content for article (Q3)

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+interface Article {
+  title: string,
+  author: string, 
+  content: string, 
+  image: string, 
+  imageAlt: string,
+  isPublished: boolean, 
+  comment: string,
+}
 
 @Component({
   selector: 'app-article',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './article.component.html',
   styleUrl: './article.component.scss'
 })
-export class ArticleComponent {
 
+export class ArticleComponent {
+  article: Article = {
+    title: 'Adventure Time',
+    author: 'Finn', 
+    content: 'Come on, grab your friend, we\'ll go to very distant lands!', 
+    image: 'assets/JakeTheDog.jpg', 
+    imageAlt: 'Jake. He\'s a dog.',
+    isPublished: true, 
+    comment: '',
+  }
+
+  togglePublishing(): void {
+    this.article.isPublished = !this.article.isPublished;
+  }
 }
