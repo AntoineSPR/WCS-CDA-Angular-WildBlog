c9b231215f2ff63fc703d70821345a80ac4185ab
AntoineSPR <antoine.simper@gmail.com>
2024-09-30 09:30:07 +0200

    Add list of articles (Q4)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,16 @@ import { CalculatriceComponent } from "./calculatrice/calculatrice.component";
 import { CatalogComponent } from "./catalog/catalog.component";
 import { CommonModule } from '@angular/common';
 
+interface Article {
+  title: string,
+  author: string,
+  content: string,
+  image: string,
+  isPublished: boolean,
+  comment: string,
+  likes: number,
+}
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -14,4 +24,33 @@ import { CommonModule } from '@angular/common';
 })
 export class AppComponent {
   title = 'Antoine';
+  articles: Article[] = [
+    {
+      title: 'Angular 16: Les nouveautés',
+      author: 'Alice',
+      content: 'Les nouveautés d\'Angular 16 incluent...',
+      image: 'https://via.placeholder.com/350x150',
+      isPublished: true,
+      comment: '',
+      likes: 120
+    },
+    {
+      title: 'Développer une API REST',
+      author: 'Bob',
+      content: 'Développer une API REST nécessite...',
+      image: 'https://via.placeholder.com/350x150',
+      isPublished: true,
+      comment: '',
+      likes: 75
+    },
+    {
+      title: 'Pourquoi TypeScript est essentiel ?',
+      author: 'Charlie',
+      content: 'TypeScript apporte de la robustesse...',
+      image: 'https://via.placeholder.com/350x150',
+      isPublished: true,
+      comment: '',
+      likes: 200
+    }
+  ];
 }
