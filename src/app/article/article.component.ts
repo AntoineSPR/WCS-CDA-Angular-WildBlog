8a729031ab1ebc59a125b14d968946f8ca6677e8
AntoineSPR <antoine.simper@gmail.com>
2024-10-04 12:57:40 +0200

    Add routes and input article from parent

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,15 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
-interface Article {
-  title: string,
-  author: string, 
-  content: string, 
-  image: string, 
-  imageAlt: string,
-  isPublished: boolean, 
-  comment: string,
-}
+import { Article } from '../home-page/home-page.component';
 
 @Component({
   selector: 'app-article',
@@ -20,15 +11,7 @@ interface Article {
 })
 
 export class ArticleComponent {
-  article: Article = {
-    title: 'Adventure Time',
-    author: 'Finn', 
-    content: 'Come on, grab your friend, we\'ll go to very distant lands!', 
-    image: 'assets/JakeTheDog.jpg', 
-    imageAlt: 'Jake. He\'s a dog.',
-    isPublished: true, 
-    comment: '',
-  }
+  @Input() article!: Article;
 
   togglePublishing(): void {
     this.article.isPublished = !this.article.isPublished;
---
+++ src/app/home-page/home-page.component.ts
@@ -0,0 +1,68 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { ArticleComponent } from '../article/article.component';
+import { RouterLink } from '@angular/router';
+
+export interface Article {
+  id?: number,
+  title: string,
+  author: string,
+  content: string,
+  image: string,
+  imageAlt?: string,
+  isPublished: boolean,
+  comment: string,
+  likes?: number,
+}
+
+@Component({
+  selector: 'app-home-page',
+  standalone: true,
+  imports: [CommonModule, RouterLink, ArticleComponent],
+  templateUrl: './home-page.component.html',
+  styleUrl: './home-page.component.scss'
+})
+export class HomePageComponent {
+  jakeArticle: Article = {
+    title: 'Adventure Time',
+    author: 'Finn', 
+    content: 'Come on, grab your friend, we\'ll go to very distant lands!', 
+    image: 'assets/JakeTheDog.jpg', 
+    imageAlt: 'Jake. He\'s a dog.',
+    isPublished: true, 
+    comment: '',
+  }
+
+  articles: Article[] = [
+    {
+      id: 1,
+      title: 'Angular 16: Les nouveautés',
+      author: 'Alice',
+      content: 'Les nouveautés d\'Angular 16 incluent...',
+      image: 'https://via.placeholder.com/350x150',
+      isPublished: true,
+      comment: '',
+      likes: 120
+    },
+    {
+      id: 2,
+      title: 'Développer une API REST',
+      author: 'Bob',
+      content: 'Développer une API REST nécessite...',
+      image: 'https://via.placeholder.com/350x150',
+      isPublished: true,
+      comment: '',
+      likes: 75
+    },
+    {
+      id: 3,
+      title: 'Pourquoi TypeScript est essentiel ?',
+      author: 'Charlie',
+      content: 'TypeScript apporte de la robustesse...',
+      image: 'https://via.placeholder.com/350x150',
+      isPublished: true,
+      comment: '',
+      likes: 200
+    }
+  ];
+}
