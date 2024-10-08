5772cb39afc289adf0c56e23730d35c1bf91272b
Antoine Simper <antoine.simper@beecoming.fr>
2024-10-08 11:17:28 +0200

    Edit contact info when form submitted

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ContactInfo } from '../models/contact-info.model';
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.scss'
 })
 export class ContactFormComponent {
 
+  contactInfo: ContactInfo = {
+    firstname: "",
+    lastname: "", 
+    email: ""
+  }
+
+  onSubmit(): void {
+    console.log("Formulaire envoyé, mon vieux.");
+    Object.entries(this.contactInfo).forEach(
+      ([key, value]) => {
+        console.log(`${key} : ${value}`);
+      }
+    )
+  }
 }
---
+++ src/app/models/contact-info.model.ts
@@ -0,0 +1,5 @@
+export type ContactInfo = {
+    firstname: string;
+    lastname: string;
+    email: string;
+}
