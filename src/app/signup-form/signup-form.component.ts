9da5e6b565138d51c6d1ff05e797815314abb5fe
Antoine Simper <antoine.simper@beecoming.fr>
2024-10-08 16:36:30 +0200

    Add username and email

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './signup-form.component.html',
   styleUrl: './signup-form.component.scss'
 })
 export class SignupFormComponent {
+  formBuilder = inject(FormBuilder);
 
+  signUpForm = this.formBuilder.group({
+    username: ['', [Validators.required, Validators.minLength(3)]], 
+    email: ['', [Validators.required, Validators.email]],
+  })
+
+  onSubmit(): void {
+    if (this.signUpForm.valid) {
+      console.log('Et un gobelin de plus dans mon armée !', this.signUpForm.value);
+    } else {
+      console.log('Kobold en vue, sortez les arbalètes.')
+    }
+  }
 }
