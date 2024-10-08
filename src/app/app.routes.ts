ba1833e0a2796e397ab47f3e633dd00e3b42d3ee
Antoine Simper <antoine.simper@beecoming.fr>
2024-10-08 11:01:53 +0200

    Create contact form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { ArticlePageComponent } from './article-page/article-page.component';
 import { WorkshopsComponent } from './workshops/workshops.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
 export const routes: Routes = [
     { path: '', component: HomePageComponent },
     { path: 'article/:id', component: ArticlePageComponent },
     { path: 'workshops', component: WorkshopsComponent },
+    { path: 'contact', component: ContactFormComponent },
     { path: '**', component: NotFoundComponent},
 ];
