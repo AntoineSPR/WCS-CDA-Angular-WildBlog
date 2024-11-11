38d8e014e9c7b8c2dd1a0ceebb4477addd33aeb8
AntoineSPR <antoine.simper@gmail.com>
2024-11-11 16:47:03 +0100

    Add sign-up reactive form

---
+++ src/app/signup-form/signup-form.component.ts
@@ -0,0 +1,54 @@
+import { Component, inject } from '@angular/core';
+import { AbstractControl, FormBuilder, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-signup-form',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './signup-form.component.html',
+  styleUrl: './signup-form.component.scss'
+})
+export class SignupFormComponent {
+  fb = inject(FormBuilder);
+
+  signupForm = this.fb.group({
+    username: ['', [Validators.required, Validators.minLength(3)]],
+    email: ['', [Validators.required, Validators.email]],
+    passwords: this.fb.group({
+      password: ['', [Validators.required, this.securePasswordValidator()]],
+      confirmPassword: ['']
+    }, { validators: this.passwordMatchValidator() })
+  });
+
+  onSubmit(): void {
+    if (this.signupForm.valid) {
+      console.log("Formulaire envoyé : ", this.signupForm.value);
+    } else {
+      console.log("Invalide");
+    }
+  }
+
+  securePasswordValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const value = control.value || '';
+
+      const hasUppercase = /[A-Z]/.test(value);
+      const hasLowercase = /[a-z]/.test(value);
+      const hasNumber = /\d/.test(value);
+      const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(value);
+      const isValidLength = value.length >= 12;
+
+      const passwordValid = hasUppercase && hasLowercase && hasNumber && hasSpecialChar && isValidLength;
+
+      return passwordValid ? null : { securePassword: true };
+    }
+  }
+
+  passwordMatchValidator(): ValidatorFn {
+    return (formGroup: AbstractControl): ValidationErrors | null => {
+      const password = formGroup.get('password')?.value;
+      const confirmPassword = formGroup.get('confirmPassword')?.value;
+      return password === confirmPassword ? null : { passwordsMismatch: true };
+    };
+  }
+}
